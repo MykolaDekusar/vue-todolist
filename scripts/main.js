48eeec9ab4a2946076e40225966eac8ef4254ff9
MykolaDekusar <ndekusar97@gmail.com>
2024-05-22 18:10:19 +0200

    Milestone 1 completed

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      message: "",
+      list: [],
+      initList: 0,
+      circle_color: "red_circle",
+    };
+  },
+  methods: {
+    addText() {
+      if (this.message !== "") {
+        this.list[this.initList] = { text: this.message, done: false };
+        this.initList++;
+      }
+      this.message = "";
+      console.log(this.initList);
+      document.getElementById("taskInput").value = "";
+    },
+    removeText() {
+      this.list.splice(this.initList - 1, 1);
+      if (this.list.length > 0) this.initList--;
+    },
+    removeSelectedText(index) {
+      console.log(this.list);
+      this.list.splice(index, 1);
+      this.initList--;
+    },
+    updMessage(value) {
+      console.log(value);
+      this.message = value;
+    },
+    completedTask(index) {
+      this.list[index].done = true;
+    },
+  },
+}).mount("#app");
